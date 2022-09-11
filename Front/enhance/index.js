cd76f1ef9dafe3da5fafafc8ff5cd9a78d5ad787
Ar-Sr-Na <arsrna.cn@outlook.com>
2022-09-11 14:07:47 +0800

    背景功能

--- Front/enhance/index.js
+++ Front/enhance/index.js
@@ -80,3 +80,31 @@ $('#logProgress').html(`<p id="logs" class="col lead">
 
 
 }
+
+
+function changeBg(url,select,defaultBg) {
+  if(defaultBg){
+    //选择默认背景时
+    $("#bgFilePath").html('默认背景');
+    localStorage.setItem('backgroundURL','https://res.arsrna.cn/%E5%B4%A9%E5%9D%8F3/307e97670bccb52217793c37875db3b6_4575220996410919312.png_web');
+  }else{
+     $("#bgFilePath").html(url);
+     localStorage.setItem('backgroundURL',url);
+  }
+  localStorage.setItem('backgroundSwitch',select);
+  location.reload()
+}
+
+function startChangeBg() {
+  var url = localStorage.getItem('backgroundURL'),
+      open = localStorage.getItem('backgroundSwitch');
+      //背景设置部分
+  if(open=='true'){
+    $("#bgFilePath").html(url);
+    $("#bgSwitch")[0].checked = true;
+    $('.backgroundImg').attr('src',url)
+  }else{
+    $(".backgroundImg").remove();
+    $("#bgSwitch")[0].checked = false;
+  }
+}
