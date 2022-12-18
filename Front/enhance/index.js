ac425b556b0a5f48aeeb25a215cc41a9cd4d6e6b
Ar-Sr-Na <arsrna.cn@outlook.com>
2022-12-18 21:40:13 +0800

    删除冗余文件，优化主页，新增产品enhance内容

--- Front/enhance/index.js
+++ Front/enhance/index.js
@@ -1,8 +1,9 @@
-$('#logs').hide()
-function generate(file) {
-  var fileRandomKey = `ArAI_${parseInt(Math.random()*1000)}_${new Date().getTime()}_${file.files[0].name}`
+$('#logs').hide();
+
+function upload(file,callback){
+  fileRandomKey = `ArAI_${parseInt(Math.random()*1000)}_${new Date().getTime()}_${file.files[0].name}`
   $('#logs').show()
-  $('#logs').html(`ArAI 提交中`);
+  $('#logs').html(`ArAI 文件上传中`);
   cos.putObject({
     Bucket: Bucket, /* 必须 */
     Region: Region,     /* 存储桶所在地域，必须字段 */
@@ -21,45 +22,53 @@ function generate(file) {
     if(err){
       alert(`错误：${err}`)
     }
+    //执行回调
+    callback(fileRandomKey)
+    //tiia评估质量
     COSDownload(`/${fileRandomKey}`,'',(msg)=>{
       $('#origin').attr('src',msg);
       tiiaAnalysis('AssessQuality',msg,(msg)=>{
         console.log(msg.responseJSON)
         var data=msg.responseJSON;
-        var temp=`<div class="lead">清晰度${data.ClarityScore}%</div><div class="progress"><div class="progress-bar"style="width: ${data.ClarityScore}%">${data.ClarityScore}%</div></div><div class="lead">美观度${data.AestheticScore}</div><div class="progress"><div class="progress-bar"style="width: ${data.AestheticScore}%">${data.AestheticScore}%</div></div><hr><table class="table"><thead><tr><th scope="col">项目</th><th scope="col">详情</th></tr></thead><tbody class="table-group-divider"><tr><th>长图</th><td>${data.LongImage}</td></tr><tr><th>黑白图</th><td>${data.BlackAndWhite}</td></tr><tr><th>小图</th><td>${data.SmallImage}</td></tr><tr><th>大图</th><td>${data.BigImage}</td></tr><tr><th>纯色图</th><td>${data.PureImage}</td></tr><tr><th>RequestId</th><td>${data.RequestId}</td></tr></tbody></table>`;
+        var temp=`<div class="lead">清晰度${data.ClarityScore}%</div><div class="progress">
+        <div class="progress-bar"style="width: ${data.ClarityScore}%">${data.ClarityScore}%</div>
+        </div><div class="lead">美观度${data.AestheticScore}</div><div class="progress">
+        <div class="progress-bar"style="width: ${data.AestheticScore}%">${data.AestheticScore}%</div></div>
+        <hr>
+        <table class="table"><thead><tr><th scope="col">项目</th><th scope="col">详情</th></tr></thead><tbody class="table-group-divider"><tr><th>长图</th><td>${data.LongImage}</td></tr><tr><th>黑白图</th><td>${data.BlackAndWhite}</td></tr><tr><th>小图</th><td>${data.SmallImage}</td></tr><tr><th>大图</th><td>${data.BigImage}</td></tr><tr><th>纯色图</th><td>${data.PureImage}</td></tr><tr><th>RequestId</th><td>${data.RequestId}</td></tr></tbody></table>`;
        $('#imgAnalysis').html(temp)
       })
     });
-    
-    COSDownload(`/${fileRandomKey}`,{'imageMogr2/sharpen/1000':''},(msg)=>{
-        tiiaAnalysis('EnhanceImage',msg,(msg)=>{
-          if(msg.status==200){
-            var body = dataURLtoBlob(`data:image/png;base64,${msg.responseJSON.EnhancedImage}`);
-            cos.putObject({
-                Bucket: Bucket, /* 填入您自己的存储桶，必须字段 */
-                Region: Region,  /* 存储桶所在地域，例如ap-beijing，必须字段 */
-                Key: `enhanced_${fileRandomKey}`,  /* 存储在桶里的对象键（例如1.jpg，a/b/test.txt），必须字段 */
-                Body: body,
-            }, function(err, data) {
-                console.log(err || data);
-                COSDownload(`/enhanced_${fileRandomKey}`,'',(msg)=>{
-                  $('#process').attr('src',msg)
-                  $('#AfterDownload').removeAttr('disabled')
-                  $('#AfterDownload').attr('href',msg+`&response-content-disposition=attachment`)
-                  $('#logProgress').html('')
-                })
-            });
-          //console.log(msg)
-          
-      }else{
-        alert(`处理失败 ${msg}`)
-      }
-    })
-    
-    });
-
-
 });
+}
+
+var generate={
+  2:function() {
+    COSDownload(`/${fileRandomKey}`,{
+      'ci-process=AIEnhanceImage':'',
+      denoise:$('#denoise').val(),
+      sharpen:$('#sharp').val()
+    },
+    function(msg){
+      $('#process').attr('src',msg);
+      $('#logProgress').hide()
+      $('#AfterDownload').attr('href',msg)
+    })
+  },
+
+  1:function() {
+    COSDownload(`/${fileRandomKey}`,{
+      'ci-process':'AISuperResolution',
+    },
+    function(msg){
+      $('#process').attr('src',msg);
+      $('#logProgress').hide()
+      $('#AfterDownload').attr('href',msg)
+    })
+  },
+}
+
+
 
 function tiiaAnalysis(path,imageURL,callback){
    var ajaxbase = $.ajax({
@@ -72,13 +81,8 @@ function tiiaAnalysis(path,imageURL,callback){
 $('#logProgress').html(`<p id="logs" class="col lead">
         ArAI TIIA处理中</p>
         <div class="spinner-border ms-auto col-1" role="status" aria-hidden="true"></div>`)
-
       try{callback(ajaxbase)}
       catch(err){console.warn('TIIA回调错误 '+err)}
-}
-
-
-
 }
 
 
@@ -106,5 +110,17 @@ function startChangeBg() {
   }else{
     $(".backgroundImg").remove();
     $("#bgSwitch")[0].checked = false;
+  }
+}
+
+
+function changeOptions(val){
+  switch(val){
+    case '1'||'3':
+      $('.optRange').attr('disabled','')
+    break;
+    case '2':
+      $('.optRange').removeAttr('disabled')
+    break
   }
 }
