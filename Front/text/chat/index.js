c0fa031349fcf3481f168de57efc79ae0bff8e6c
Ar-Sr-Na <arsrna.cn@outlook.com>
2022-12-23 18:53:16 +0800

    聊天功能接入

--- Front/text/chat/index.js
+++ Front/text/chat/index.js
@@ -1,51 +1,65 @@
-function submit(){
-  generateLoad('查询中','.logProgress')
+function getKey() {
+  if (event.keyCode == 13) {
+    send($('#sendMsg').val());
+  }
+}
 
-      query('tia',{
-        text:$('#mainText').val(),
-        num:$('#tiaNum').val()
+function send(text){
+  generateLoad('等待中','.logProgress')
+  if(text==''){alert('请输入内容')}else{
+    addMsg.self(text)
+    $.ajax({
+      url:'https://api.ai.arsrna.cn/release/NLP',
+      data:{
+       mode:'chat',
+       text:text,
+       flag:$('#mode').val()
+     },
+      headers:{
+        token:tokenAll.access_token
       },
-        function(msg){
-          var temp=[{"<>":"div","class":"lead","style":"font-size:calc(200%*${Score})","html":"${Word}"},
-            {"<>":"div","class":"progress mb-2","html":[
-            {"<>":"div","class":"progress-bar","role":"progressbar","style":"width: calc(100%*${Score});","html":"${Score}"}
-          ]}];
-          var data=msg.responseJSON.data
-          //console.log(data)
-          if(msg.responseJSON.success){
-            $('#tiaAnalysis').html(json2html.transform(data.Keywords,temp))
-          }else{
-            alert(`错误：${data}`)
-          }
-        }
-      )
-
-      query('digest',{
-        text:$('#mainText').val(),
-        length:$('#digestLength').val()
-      },
-        function(msg){
-          var data=msg.responseJSON.data
-          //console.log(data)
-          if(msg.responseJSON.success){
-            $('#digestAnalysis').html(data.Summary)
-          }else{
-            alert(`错误：${data}`)
-          }
-        }
-      )
+      dataType:'json',
+      success(msg){
+       console.log(msg)
+       addMsg.bot(msg)
+       $('.logProgress').html('')
+      }
+   }) 
+  }
+  
   }
 
 
-function query(mode,args,callback){
-  var ajaxbase=$.ajax({
-        url:'https://api.ai.arsrna.cn/release/NLP',
-        data:Object.assign({mode:mode},args),
-        headers:{
-          token:tokenAll.access_token
-        },
-        dataType:'json',
-        async:false
-}) 
-callback(ajaxbase)
+var addMsg={
+  self:function(msg) {
+  var html = $('#msg').html()
+  var tempSelf = `<div style="padding-top:5px" class="text-primary">
+  <small>${moment(new Date().getTime()).format('HH:mm:ss')}</small>
+  <span>我</span>
+  <div class="d-flex">
+  <div class="col-auto">
+  <i class="fa-solid fa-user fa-fw" style="padding-right:5px"></i>
+  </div>
+  <div class="col-11">
+  <span class="lead">${msg}</span>
+  </div></div></div>`;
+  $('#msg').html(tempSelf+html)
+  },
+
+  bot:function(msg){
+  var html = $('#msg').html()
+  var tempOther = `<div style="padding-top:5px">
+  <small class="text-muted">${moment(new Date().getTime()).format('HH:mm:ss')}</small>
+  <span class="text-muted">ArAI</span>
+  <span class="badge bg-primary">${(msg.data.Confidence*100).toFixed(2)}</span>
+  <div class="d-flex">
+  <div class="col-auto">
+  <i class="fa-solid fa-robot fa-fw" style="padding-right:5px"></i>
+  </div>
+  <div class="col-11">
+  <span class="lead">${msg.data.Reply}</span>
+  </div></div></div>`;
+  $('#msg').html(tempOther+html)
+  }
+  
 }
