2a87dd7f3a57c6bedf2bca2a6b7cd0043ddb05fe
ArSrNa <arsrna.cn@outlook.com>
2023-03-04 21:12:17 +0800

    保存会话，并且支持3.5模型

--- Front/chatgpt/index.js
+++ Front/chatgpt/index.js
@@ -1,3 +1,5 @@
+var apiLink = ['api.openai.com/v1/completions', 'chat.openai.run/wp-json/ai-chatbot/v1/chat'];
+var apiIndex;
 function callCHATGPT() {
     if ($('#key').val() == '') { alert('请输入OpenAI API秘钥'); return }
     localStorage.setItem('question', $("#chat-gpt-input").val())
@@ -6,42 +8,39 @@ function callCHATGPT() {
     $('#chatgpt-response').html(`请求中......<br><div class="arloadLine answerLoad"><div></div></div>`);
     $('#submitBtn').addClass('disabled');
     $('.answerLoad').show();
-
+    var args = [{
+        "session": "N/A",
+        "prompt": $("#chat-gpt-input").val(),
+        "max_tokens": 4000,
+        "temperature": 0.8,
+        "top_p": 1,
+        "frequency_penalty": 0,
+        "presence_penalty": 0,
+        "model": 'gpt-3.5-turbo'
+    }, {
+        "env": "chatbot",
+        "session": "N/A",
+        "prompt": $("#chat-gpt-input").val(),
+        "model": "gpt-3.5-turbo",
+        "temperature": 0.8,
+        "maxTokens": 1024,
+        "maxResults": 1
+    }]
     $.ajax({
-        url: "https://api.openai.com/v1/completions",
+        url: `https://${apiLink[apiIndex]}`,
         type: 'POST',
         headers: {
             'Content-Type': "application/json",
             'Authorization': `Bearer ${$('#key').val()}`
         },
-        data: JSON.stringify({
-            "prompt": $("#chat-gpt-input").val(),
-            "max_tokens": 2048,
-            "temperature": 0.5,
-            "top_p": 1,
-            "frequency_penalty": 0,
-            "presence_penalty": 0,
-            "model": $('#model').val()
-        }),
+        data: JSON.stringify(args[apiIndex]),
 
         success(data) {
-            console.log(data);
-            var htmlMark = marked.parse(data.choices[0].text);
-            $('#chatgpt-response').html(htmlMark);
-            hljs.highlightAll();
-            localStorage.setItem('lastConv',htmlMark);
-  
-            $('#req').html(`本次对话ID：${data.id}<br>用量：${data.usage.total_tokens}`);
-            $('#submitBtn').removeClass('disabled');
-            $('.answerLoad').hide();
-            viewGrants()
+            show(apiIndex, data)
         },
 
         error(data) {
-            $('#submitBtn').removeClass('disabled');
-            $('#req').html(`错误请求`);
-            $('#chatgpt-response').html(data);
-            hljs.highlightAll();
+            showErr(data)
         }
 
     })
@@ -50,10 +49,53 @@ function callCHATGPT() {
 }
 
 
+function show(apiIndex, data) {
+    console.log(data);
+    switch (apiIndex) {
+        case 0:
+            var htmlMark = marked.parse(data.choices[0].text);
+            $('#chatgpt-response').html(htmlMark);
+            hljs.highlightAll();
+            localStorage.setItem('lastConv', htmlMark);
+            $('#req').html(`本次对话ID：${data.id}<br>用量：${data.usage.total_tokens}`);
+            viewGrants();
+            break;
+
+        case 1:
+            var htmlMark = marked.parse(data.answer);
+            $('#chatgpt-response').html(htmlMark);
+            hljs.highlightAll();
+            localStorage.setItem('lastConv', htmlMark);
+            $('#req').html(`本次用量：${data.usage.total_tokens}`);
+            break;
+    }
+
+
+
+
+
+    $('#submitBtn').removeClass('disabled');
+    $('.answerLoad').hide();
+
+}
+
+function showErr(data) {
+    console.log(data)
+    $('#submitBtn').removeClass('disabled');
+    $('#chatgpt-response').html(JSON.stringify(data));
+    layer.open({
+        title: "发生错误",
+        content: `${JSON.stringify(data)}
+                <hr>若错误与http报错状态码相关，请检查网络是否顺畅。
+                `,
+    });
+    hljs.highlightAll();
+}
+
 function viewGrants() {
     $('.balance').html(`<div class="arloadLine"><div></div></div>`);
     $.ajax({
-        url: 'https://api.openai.com/dashboard/billing/credit_grants',
+        url: `https://${apiLink}/dashboard/billing/credit_grants`,
         type: 'GET',
         headers: {
             'Content-Type': "application/json",
@@ -64,6 +106,14 @@ function viewGrants() {
             $('#total').html('$ ' + msg.total_granted);
             $('#used').html('$ ' + msg.total_used);
             $('#total_available').html('$ ' + msg.total_available);
+        },
+        error(data) {
+            layer.open({
+                title: "发生错误",
+                content: `${JSON.stringify(data)}
+                <hr>若错误与http报错状态码相关，请检查网络是否顺畅。
+                `,
+            });
         }
     })
 };
